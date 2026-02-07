033d33f44b5408aa5abd5f6ea77201b92aa45bbc
Raymond Kwok <rmwkwok@gmail.com>
2026-02-07 16:38:51 +0800

    automatically get dates for pages

--- _plugins/git_dates.rb
+++ _plugins/git_dates.rb
@@ -1,9 +1,10 @@
-Jekyll::Hooks.register :posts, :pre_render do |post|
-  # Get the creation date (first commit of the file)
-  creation_time = `git log --follow --format=%ad --date=iso -- "#{post.path}" | tail -1`.strip
-  post.data['date'] = creation_time unless creation_time.empty?
+Jekyll::Hooks.register [:posts, :pages], :pre_render do |doc|
+  # Only process Markdown or HTML files
+  next unless doc.extname =~ /^\.(md|html)$/
 
-  # Get the last modified date (last commit of the file)
-  modification_time = `git log -1 --format=%ad --date=iso -- "#{post.path}"`.strip
-  post.data['last_modified_at'] = modification_time unless modification_time.empty?
+  creation_time = `git log --follow --format=%ad --date=iso -- "#{doc.path}" | tail -1`.strip
+  doc.data['date'] = creation_time unless creation_time.empty?
+
+  mod_time = `git log -1 --format=%ad --date=iso -- "#{doc.path}"`.strip
+  doc.data['last_modified_at'] = mod_time unless mod_time.empty?
 end
