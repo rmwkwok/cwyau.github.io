88bf947186f005b2c7b43a153ac5589a6e7201cc
Raymond Kwok <rmwkwok@gmail.com>
2026-02-07 14:07:04 +0800

    remove unnecessary console log

--- assets/js/tilesrow.js
+++ assets/js/tilesrow.js
@@ -4,11 +4,9 @@ const options = {
   threshold: 0.6  // 60% of the screen to activate
 };
 
-console.log('init. adding active');
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      console.log('adding active');
       tiles.forEach(t => t.classList.remove('active'));
       entry.target.classList.add('active');
     }
